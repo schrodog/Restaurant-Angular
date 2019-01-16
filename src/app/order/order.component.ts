67b9dda5b90e74dd2aea9417bd156b5553ce6493
schrodog <sky_sun_tg@hotmail.com>
2019-01-16 23:36:34 +0800

    new

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -10,11 +10,10 @@ import { DataService } from '../services/data.service';
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, OnChanges {
 
-  // @Input('masterOrderID') masterOrderID: number;
-  // @Input('tableNo') tableNo: string;
-  @Output('orders') addOrderTable = new EventEmitter<any[]>();
+  @Input() currentMasterOrderID: number;
+  @Input() currentTableNo: string;
 
   constructor(
     private orderService: OrderService, 
@@ -22,8 +21,9 @@ export class OrderComponent implements OnInit {
     private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.currentOrders.subscribe(x => this.orders = x)
+    this.dataService.currentOrders.subscribe(x => this.setOrders(x))
   }
+
 
   fields: string[] = ["Code", "Food Name", "Quantity", "Price"]
   orders: Order[];
@@ -31,7 +31,12 @@ export class OrderComponent implements OnInit {
   totalPrice: number = 0;
   moneyReceived: number = 0;
   change: number = 0;
-
+  
+  setOrders(inp: Order[]) {
+    this.totalPrice = inp.map(x => x.price).reduce( (a,b) => a + b ,0)
+    this.orders = inp
+  }
+  
   // addOrders(order: Order, masterOrderID: number){
   //   this.orderService.addNewOrder(order, masterOrderID).subscribe(
   //     () => { this.addOrderTable.emit({order}) }
